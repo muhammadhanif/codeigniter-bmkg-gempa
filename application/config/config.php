f1c478f4b909655964c4150593312730611cffb9
hanif <list>
2020-07-20 07:35:56 +0700

    for testing purpose using PHPunit

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+
+if (php_sapi_name() == "cli") {
+    // for testing purpose using PHPunit
+    $_SERVER['HTTP_HOST'] = 'dev.local';
+}
+
 $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 $config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
