716a180d80221204692dcb38118b1dcb1bb62398
hanif <list>
2020-07-28 06:05:15 +0700

    ubah nilai return fungsi

--- application/controllers/ApiV1.php
+++ application/controllers/ApiV1.php
@@ -13,21 +13,41 @@ class ApiV1 extends CI_Controller
 
     public function gempaM5Terkini()
     {
-        return $this->BMKG_v1->getGempaM5Terkini();
+
+        return $this->output->set_header('HTTP/1.0 200 OK')
+            ->set_header('HTTP/1.1 200 OK')
+            ->set_status_header(200)
+            ->set_content_type('application/json')
+            ->set_output(json_encode($this->BMKG_v1->getGempaM5Terkini()));
     }
 
     public function gempaM5()
     {
-        return $this->BMKG_v1->getGempaM5();
+
+        return $this->output->set_header('HTTP/1.0 200 OK')
+            ->set_header('HTTP/1.1 200 OK')
+            ->set_status_header(200)
+            ->set_content_type('application/json')
+            ->set_output(json_encode($this->BMKG_v1->getGempaM5()));
     }
 
     public function gempaDirasakan()
     {
-        return $this->BMKG_v1->getGempaDirasakan();
+
+        return $this->output->set_header('HTTP/1.0 200 OK')
+            ->set_header('HTTP/1.1 200 OK')
+            ->set_status_header(200)
+            ->set_content_type('application/json')
+            ->set_output(json_encode($this->BMKG_v1->getGempaDirasakan()));
     }
 
     public function gempaTsunamiTerkini()
     {
-        return $this->BMKG_v1->getGempaTsunamiTerkini();
+
+        return $this->output->set_header('HTTP/1.0 200 OK')
+            ->set_header('HTTP/1.1 200 OK')
+            ->set_status_header(200)
+            ->set_content_type('application/json')
+            ->set_output(json_encode($this->BMKG_v1->getGempaTsunamiTerkini()));
     }
 }
